7e4dc9d819fafca3e008ff84bba0d564c95f8b7d
Marina Home <golubeva.webmaster@gmail.com>
2020-10-07 12:51:23 +0400

    feat: task is completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let newBC = bracketsConfig.map(item => item.join(''));
+    let lengthOld = str.length;
+    while(lengthOld > 0){
+      newBC.forEach(function(item){
+        str = str.replace(item,'');
+      });
+      if (str.length === lengthOld)
+        return false;
+      else if(str.length === 0)
+        return true;
+      else
+        lengthOld = str.length;
+    }
 }
